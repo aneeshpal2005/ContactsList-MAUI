86c9b89b8d3080191605d5371db73a2be0591246
Aneesh Palande <45935068+aneeshpal2005@users.noreply.github.com>
2026-06-23 14:30:22 -0400

    Add project files.

---
+++ ContactsList/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactsList
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ContactsList/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ContactsList
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
